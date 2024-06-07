94adc8bb83d23559fb24e2f88dfbfe785c6f6bd5
safaa-mammed <safaa.mammed@gmail.com>
2024-06-08 00:10:13 +0800

    project updates(certs2)

--- components/CertCard.jsx
+++ components/CertCard.jsx
@@ -6,7 +6,7 @@ const CertCard = ({name,link,desc,img}) => {
     <div>
         <div className="my-8 max-w-sm max-h-sm flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-max sm:max-w-max hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 shadow-lg dark:bg-gray-800 hover:shadow-slate-500 transition duration-300 ease-in-out hover:scale-105">
         {img == "gcp" && 
-        <Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/gcp.png" alt="image"/>
+        <Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/GCP.png" alt="image"/>
         }
         {img == "ccna" && 
         <Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/ccna.png" alt="image"/>
