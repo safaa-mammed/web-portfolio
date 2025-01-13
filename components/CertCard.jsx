08ca1488cf2699ab4e3498b77fc6325088c85591
safaa-mammed <73444223+safaa-mammed@users.noreply.github.com>
2025-01-13 14:20:31 +0000

    update links

--- components/CertCard.jsx
+++ components/CertCard.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const CertCard = ({name,link,desc,img}) => {
   return (
     <div>
-        <div onClick={()=> window.open(link,'_blank')} className="my-8 max-w-sm max-h-sm flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-max sm:max-w-max hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 shadow-lg dark:bg-gray-800 hover:shadow-slate-500 transition duration-300 ease-in-out hover:scale-105">
+        <div className="my-8 max-w-sm max-h-sm flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-max sm:max-w-max hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 shadow-lg dark:bg-gray-800 hover:shadow-slate-500 transition duration-300 ease-in-out hover:scale-105">
         {img == "gmp" && 
-        <Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/GMP.png" alt="image"/>
+        <Link href={link}><Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/GMP.png" alt="image"/>
+        </Link>
         }
         {img == "pde" && 
-        <Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/PDE.png" alt="image"/>
+        <Link href={link}><Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/PDE.png" alt="image"/>
+        </Link>
         }
         {img == "gcp" && 
-        <Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/GCP.png" alt="image"/>
+        <Link href={link}><Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/GCP.png" alt="image"/>
+        </Link>
         }
         {img == "ccna" && 
-        <Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/ccna.png" alt="image"/>
+        <Link href={link}><Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/ccna.png" alt="image"/>
+        </Link>
         }
         {img == "aws" && 
-        <Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/aws.png" alt="image"/>
+        <Link href={link}><Image className="object-cover rounded-t-lg md:h-auto md:w-auto sm:h-auto sm:w-auto md:rounded-none md:rounded-s-lg" width={200} height={200} quality={100} priority src="/assets/images/aws.png" alt="image"/>
+        </Link>
         }
         <div className='flex flex-col justify-between p-4 leading-normal md:max-w-3xl'>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
