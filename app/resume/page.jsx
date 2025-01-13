719ddf4f6012e439a8ef5361611144af87a28d74
safaa-mammed <73444223+safaa-mammed@users.noreply.github.com>
2025-01-13 20:36:50 +0800

    Update resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -85,81 +85,8 @@ Dynamic Yield, Heap, AppsFlyer, etc.</li>
                         </div>
                     </div>
                 </div>
-
-
-
-{/*                 
-                <div className="flex items-center relative">
-                    <div className="hidden md:block w-20">
-                        <div className="font-bold italic">2022</div>
-                        <div className="md:flex space-x-1 text-xs">
-                            <div>Jul</div>
-                            <div>-</div>
-                            <div>Sept</div>
-                        </div>                        
-                    </div>
-                    
-                    <div className="border-r-2 border-black absolute h-full left-1 md:left-20 top-2 z-10">
-                        <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-                    </div>
-
-                    <div className="ml-10">
-                        <div className="font-bold">Digital Content Creator</div>
-                        <div className="italic md:mb-4">
-                          <Link className='hover:underline hover:text-blue-600' href={'https://www.instagram.com/kabaabish_nilai/'} target="_blank">Kababish Restaurant</Link>
-                          </div>
-                        <div className="mb-4 mt-2 md:hidden">
-                            <div className="font-bold">2022</div>
-                            <div className="text-xs">Jul - Sept</div>
-                        </div>
-                        <div className="mb-10">
-                          <ul className='list-disc mx-5'>
-                            <li>Manage social media presence and content production.</li>
-                            <li>Provided marketing advise and solutions, working closely with the founders</li>
-                            <li>Promoted content through social media pages such as Facebook Instagram, Google</li>
-                          </ul>
-                        </div>
-                        </div>
-                </div>
-
-
-
-                
-                <div className="flex items-center relative">
-                    <div className="hidden md:block w-20">
-                        <div className="font-bold italic">2021</div>
-                        <div className="md:flex space-x-1 text-xs">
-                            <div>Sept</div>
-                            <div>-</div>
-                            <div>Feb</div>
-                        </div>                        
-                    </div>
-                    
-                    <div className="border-r-2 border-black absolute h-full left-1 md:left-20 top-2 z-10">
-                        <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-                        <i className="fas fa-circle bottom-0 -ml-2 absolute"></i>
-                    </div>
-
-                    <div className="ml-10">
-                        <div className="font-bold">HITer</div>
-                        <div className="italic md:mb-4">
-                          <Link  className='hover:underline hover:text-blue-600' href={'https://www.isahit.com/'} target="_blank">
-                          ISAHIT
-                          </Link>
-                          </div>
-                        <div className="mb-4 mt-2 md:hidden">
-                            <div className="font-bold">2021</div>
-                            <div className="text-xs">Sept - Feb</div>
-                        </div>
-                        <div className="mb-10">
-                          <ul className='list-disc mx-5'>
-                            <li>Perform research on given companies to extract and update their information with accuracy.</li>
-                            <li>Perform transcription of invoices used to train AI.</li>
-                            <li>Earned Top 3 HITer (ISAHIT worker) for 2 months.</li>
-                          </ul>
-                        </div>
-                      </div>
-                </div>
+               
+               
 
 
             </div>
