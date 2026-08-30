0bbfca31d622a2345524c8049c7fa4e54ab1606b
safaa-mammed <safaa.mammed@gmail.com>
2026-08-30 13:39:42 +0800

    add GA4 tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import Nav from '@/components/Nav';
+import Script from 'next/script';
+// import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata = {
     title: "My Portoflio",
@@ -9,12 +11,30 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
+        <head>
+            {/* <!-- Google Tag Manager --> */}
+        <Script defer id="gtm-script" strategy="afterInteractive">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5G5MWNB2');
+        `}
+        </Script>
+        {/* <!-- End Google Tag Manager --> */}
+        </head>
         <body>
+            {/* <!-- Google Tag Manager (noscript) --> */}
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5G5MWNB2"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            {/* <!-- End Google Tag Manager (noscript) --> */}
             <div className='main '>
                 {/* <div className='gradient'/> */}
             </div>
             <main>
                 <Nav/>
+                {/* <GoogleTagManager gtmId="GTM-5G5MWNB2"/> */}
                 <div className='flex items-center justify-center transition-opacity duration-50 '>
                     {children}
                 </div>
--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+// import { GoogleTagManager } from '@next/third-parties/google'
+export const hasDataLayer = () => typeof window !== 'undefined' && window.dataLayer;
 
 export default function Home() {
   const router = useRouter()
   return (
+    // <><GoogleTagManager gtmId="GTM-XYZ" />
     <div className="container px-6 py-10 mx-auto pt-20">
       <div className="mt-5 lg:-mx-0.5 lg:flex lg:items-center gap-20">
         <Image
@@ -13,20 +16,23 @@ export default function Home() {
           width={400}
           height={400}
           quality={100}
-          className="object-cover w-400 h-400 rounded-full border border-gray"
-        />
+          className="object-cover w-400 h-400 rounded-full border border-gray" />
         <div className="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6">
           <h1 className="block mt-4 text-3xl font-semibold text-gray-800 dark:text-white">Hello World, My name is Safaa!</h1>
           <h1 className="block mt-4 text-xl font-light text-gray-800 dark:text-white">Certified Data Engineer | Software Developer specialized in Data-Driven Analytics | Specialized in Cloud Computing</h1>
-          <br/>
-          <p className="text-justify">Throughout my career, I had the opportunity to work with various clients that ranges from insurance to aviation, providing them with helpful analytical insights driving their business decisions as well as providing support to enhance their current implementation. <br/>Apart from that, I play an important role to automate internal business processes to optimize the current workflow by building applications to improve productivity and efficiency while reducing the completion time.<br/><br/>Aside from my technical skills, I'm an adaptable quick learner with strong leadership and communication skills, being able to analyze requirements and perform as necessary to meet the required deadlines, exceeding expectations.<br/>I'm passionate in constantly learning new technologies and improving my current skillset and expertise to help grow businesses and optimize their business processes.</p>
-        <br/><br/>
-        <button onClick={() => router.push('/about')} className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-color1 rounded-lg hover:bg-color2 focus:outline-none focus:ring focus:ring-opacity-80">
-          Learn more about me!
-        </button>
+          <br />
+          <p className="text-justify">Throughout my career, I had the opportunity to work with various clients that ranges from insurance to aviation, providing them with helpful analytical insights driving their business decisions as well as providing support to enhance their current implementation. <br />Apart from that, I play an important role to automate internal business processes to optimize the current workflow by building applications to improve productivity and efficiency while reducing the completion time.<br /><br />Aside from my technical skills, I'm an adaptable quick learner with strong leadership and communication skills, being able to analyze requirements and perform as necessary to meet the required deadlines, exceeding expectations.<br />I'm passionate in constantly learning new technologies and improving my current skillset and expertise to help grow businesses and optimize their business processes.</p>
+          <br /><br />
+          <button onClick={() => {
+            router.push('/about');
+            if (hasDataLayer()) {
+              window.dataLayer.push({ event: 'link_click' });
+            }
+          }} className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-color1 rounded-lg hover:bg-color2 focus:outline-none focus:ring focus:ring-opacity-80">
+            Learn more about me!
+          </button>
         </div>
       </div>
     </div>
-    
   );
 }
